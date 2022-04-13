754a0bb4d09e75c23cf62a5cbaf91050ddc23ad7
dedenurdiyanto13 <dedenurdiyanto13@gmail.com>
2022-04-13 17:07:43 +0700

    2. Styling Component

---
+++ src/Materi/Styling/Inline/index.jsx
@@ -0,0 +1,11 @@
+const Inline = () => {
+    const style = {
+        color: "salmon",
+        fontSize: '58px'
+    }
+
+    return (
+        <h1 style={style}>Hello Eduwork</h1>
+    )
+}
+export default Inline;
